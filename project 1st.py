5785ec8ebf03a3a6d2cf53859ad62e3b75d4c394
Aon Muhammad <145316686+AonAideveloper@users.noreply.github.com>
2023-12-13 18:04:26 +0500

    Add files via upload

---
+++ project 1st.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 13 09:37:01 2023
+
+@author: web 96
+"""
+
+
+print("Welcome to our band name generator.")
+
+city = input("which city did you grow up in?\n")
+
+pet = input("what is name of pet?\n")
+
+print("your band name could be " + city + " " + pet)
+
+
+# name = input("what is your name?\n")
+# print(name)
+
+# name =input("what is your name?")
+# length = (len(name))
+# print(length)
+
